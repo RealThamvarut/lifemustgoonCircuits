49b1f92b0fced6db91fdf055f2c962770cb945d5
Nithi .P <p51104.2015@gmail.com>
2025-08-29 21:49:56 +0700

    update card reading in main (main2_test.py in RasPI)

--- RaspiConnect/main.py
+++ RaspiConnect/main.py
@@ -1,6 +1,6 @@
 from serial_handler import ESP32,send_command, receive_data, close_serial
 from databaseConnect import connectDB, getUser
-from read_card import getUID
+from read_card import RC522CardReader
 
 
 if __name__ == "__main__":
@@ -8,23 +8,32 @@ if __name__ == "__main__":
     # connectDB()
     supabase = connectDB()
     esp32 = ESP32()
-    
+    card_reader = RC522CardReader()
+
     while True:
 
-        getUID()
+        cardUID = card_reader.read_with_debounce()
+        # print("Card UID:", card_reader.translate_uid(cardUID))
+        if cardUID is not None :
+            if getUser(supabase, cardUID) != 0:
+                print(f"Found User: {cardUID}")
+                command = input("Enter command (or 'exit' to quit): ").strip()
+                if command.lower() == 'exit' or command.lower() == 'quit':
+                    break
 
-        command = input("Enter command (or 'exit' to quit): ").strip()
-        if command.lower() == 'exit' or command.lower() == 'quit':
-            break
+                send_command(command)
+                response = receive_data()
 
-        send_command(command)
-        response = receive_data()
+                if command == "getdata":
+                    esp32.getSensor()
+                    print("WaterLevel: ", esp32.waterLevel)
+                    print("Temperature: ", esp32.temperature)
+                    print("Weight", esp32.weight)
+            else:
+                print(f"User {cardUID} not found")
+        else:
+            print("Invalid user")
 
-        if command == "getdata":
-            esp32.getSensor()
-            print("WaterLevel: ", esp32.waterLevel)
-            print("Temperature: ", esp32.temperature)
-            print("Weight", esp32.weight)
-
+    
     close_serial()
     print("Connection closed!")
