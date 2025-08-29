6fafa49bcfe3c5ae52ea0374716ab7a74cd5f16b
Yurills <rawityurill@gmail.com>
2025-08-29 19:01:19 +0700

    feat: add read_card.py

--- read_card.py
+++ read_card.py
@@ -1,26 +1,13 @@
-from pirc522 import RFID
-rdr = RFID(pin_rst= 22)
-isRead = False
-
+from read_card import RC522CardReader
 try:
   while True:
     #rdr.wait_for_tag()
-    (error, tag_type) = rdr.request()
-    if not error:
-      print("Tag detected")
-      (error, uid) = rdr.anticoll()
-      if not error & (not isRead):
-        isRead = True
-        print("UID: " + str(uid))
-        # Select Tag is required before Auth
-        if not rdr.select_tag(uid):
-          # Auth for block 10 (block 2 of sector 2) using default shipping key A
-          if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
-            # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-            print("Reading block 10: " + str(rdr.read(10)))
-            # Always stop crypto1 when done working
-            rdr.stop_crypto()
+    card_reader = RC522CardReader()
+    uid = card_reader.read_with_debounce()
+    if uid is not None:
+        print("UID: " + card_reader.translate_uid(uid))
+    
 
 except KeyboardInterrupt:
   # Graceful shutdown
-  rdr.cleanup()
+  card_reader.cleanup()
