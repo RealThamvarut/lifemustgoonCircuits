b50d904abc9db786057ddb970618ee2651dbbe50
Idin <idinmaster.1004.2015@gmail.com>
2025-11-28 16:35:08 +0000

    feat: button + button collab

--- RaspiConnect/main.py
+++ RaspiConnect/main.py
@@ -2,15 +2,42 @@ from serial_handler import ESP32,send_command, receive_data, close_serial
 from databaseConnect import connectDB, getUser
 from read_card import RC522CardReader
 from led import Led
+import time
+import lgpio
 
 if __name__ == "__main__":
 
     supabase = connectDB()
+    print("esp32 test")
     esp32 = ESP32()
+    print("esp32 passed!")
     card_reader = RC522CardReader()
     led = Led()
+
+
+    CHIP = 0               # usually 0 on all Raspberry Pis
+    PUMP_PIN = 5       # output
+    BUTTON_PIN = 16       # GPIO number, not pin number
+
+    h = lgpio.gpiochip_open(CHIP)
+    lgpio.gpio_claim_output(h, PUMP_PIN)
+
+    lgpio.gpio_claim_input(h, BUTTON_PIN)
+
+
+    # Note: gpiod doesn't directly handle internal pull-up/pull-downs
+    # You may need to enable them in /boot/config.txt using dtoverlay if needed:
+    # e.g., "dtoverlay=gpio-pull,16,pull-down"
+
+    print("Monitoring button on GPIO16...")
+
+
     try:
         while True:
+            val = lgpio.gpio_read(h, BUTTON_PIN)
+            if val:
+                print("Button was pushed!")
+            time.sleep(0.1)
 
             cardUID = card_reader.read_with_debounce()
             # print("Card UID:", card_reader.translate_uid(cardUID))
@@ -20,20 +47,27 @@ if __name__ == "__main__":
                     print(f"Found User: {hexUID}")
                     led.greenOn()
 
-                    command = input("Enter command (or 'exit' to quit): ").strip()
-                    if command.lower() == 'exit' or command.lower() == 'quit':
-                        break
+                    # command = input("Enter command (or 'exit' to quit): ").strip()
+                    # if command.lower() == 'exit' or command.lower() == 'quit':
+                    #     break
 
-                    # command = "activate"
+                    command = "activate"
                     # send_command(command)
                     # response = receive_data()
+                    response = True
+
+                    if response: 
+                        lgpio.gpio_write(h, PUMP_PIN, 1)
+                        time.sleep(5)
+                        lgpio.gpio_write(h, PUMP_PIN, 0)
 
 
-                    if command == "getdata":
-                        esp32.getSensor()
-                        print("WaterLevel: ", esp32.waterLevel)
-                        print("Temperature: ", esp32.temperature)
-                        print("Weight", esp32.weight)
+
+                    # if command == "getdata":
+                    #     esp32.getSensor()
+                    #     print("WaterLevel: ", esp32.waterLevel)
+                    #     print("Temperature: ", esp32.temperature)
+                    #     print("Weight", esp32.weight)
                     led.greenOff()
                 else:
                     print(f"User {hexUID} not found")
--- RaspiConnect/read_card.py
+++ RaspiConnect/read_card.py
@@ -1,13 +1,34 @@
-from read_card import RC522CardReader
-try:
-  while True:
-    #rdr.wait_for_tag()
-    card_reader = RC522CardReader()
-    uid = card_reader.read_with_debounce()
-    if uid is not None:
-        print("UID: " + card_reader.translate_uid(uid))
-    
+from pirc522 import RFID
 
-except KeyboardInterrupt:
-  # Graceful shutdown
-  card_reader.cleanup()
+class RC522CardReader:
+    def __init__(self, pin_rst=22):
+        self.rdr = RFID(pin_rst=pin_rst)
+        self.previous_read = None
+        self.current_read = None
+        
+    def read(self) -> int:
+        (error, tag_type) = self.rdr.request()
+        if not error:
+            (error, uid) = self.rdr.anticoll()
+            if not error:
+                return uid
+        return None
+    
+    def read_with_debounce(self) -> int:
+        self.current_read = self.read()
+        if self.current_read is not None:
+            if self.previous_read is None:
+                self.previous_read = self.current_read
+                return self.current_read
+        else:
+            self.previous_read = self.current_read
+            return None
+    def translate_uid(self, uid) -> str:
+        str_uid = ""
+        for i in uid:
+            digit = str(hex(i))[2:]
+            str_uid += digit
+        return str_uid
+
+    def cleanup(self):
+        self.rdr.cleanup()
